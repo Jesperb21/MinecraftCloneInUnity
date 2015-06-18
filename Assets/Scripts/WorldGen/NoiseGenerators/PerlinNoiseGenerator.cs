f0e78783fd39848323675a7a5dc95c1010b48b99
Jesperb21 <Jesper.j.b.baunsgaard@gmail.com>
2015-06-18 15:09:35 +0200

    split world generation out in different chunks + messed up the hill generation

--- Assets/Scripts/WorldGen/NoiseGenerators/PerlinNoiseGenerator.cs
+++ Assets/Scripts/WorldGen/NoiseGenerators/PerlinNoiseGenerator.cs
@@ -4,25 +4,42 @@ using System.Collections;
 public class PerlinNoiseGenerator : MonoBehaviour {
     GameObject world;
     public GameObject block;
+    public int chunkSize = 16;
 
 	// Use this for initialization
 	void Start () {
-        for (float x = 0; x < 100; x++)
+        int hillModifier = 50;
+
+        for (int x = 0; x < 4; x++)
         {
-            for(float z = 0; z < 100; z++){
-                float y = Mathf.PerlinNoise(x/10, z/10) *20;
-                
-                GameObject c = (GameObject)Instantiate(block, new Vector3(x,y, z), Quaternion.identity);
-                c.transform.parent = transform;
-                float r = Mathf.PerlinNoise(x / 10, z / 10);
-                float g = Mathf.PerlinNoise(x / 20, z / 20);
-                float b = Mathf.PerlinNoise(x / 30, z / 30);
-                c.GetComponent<Renderer>().material.color = new Color(r, g, b);
+            for(int z = 0; z < 4; z++){
+                GameObject chunk = new GameObject("chunk_x"+x+"_z"+z);
+                chunk.transform.parent = transform;
+                chunk.transform.position = new Vector3(x * chunkSize,0f, z * chunkSize);
+                GenerateChunk(x, z, chunk, hillModifier);
+                hillModifier++;
             }
         }
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    void GenerateChunk(int chunkX,int chunkZ, GameObject chunkObject, int hillModifier)
+    {
+        int actualChunkX = chunkX * chunkSize;
+        int actualChunkZ = chunkZ * chunkSize;
+        float y;
+        //for (int layers = 0; layers < 5; layers++)
+        //{
+            for (int x = 0; x < chunkSize; x++)
+            {
+                for (int z = 0; z < chunkSize; z++)
+                {
+                    y = Mathf.PerlinNoise((float)(x + actualChunkX) / hillModifier/**layers*/, (float)(actualChunkZ + z) / hillModifier/**layers*/);
+                    y *= 25/*layers**/;
+
+                    GameObject c = (GameObject)Instantiate(block, new Vector3(actualChunkX + x, y, actualChunkZ + z), Quaternion.identity);
+                    c.transform.parent = chunkObject.transform;
+                }
+            }
+        //}
     }
 }
