e20d5ac2aa88b75a7ef4768d512140d6924a4328
JavaORayent <marita.java@gmail.com>
2015-06-18 13:03:39 +0200

    Textures Blocks

---
+++ Assets/Scripts/Block_SetMaterial.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block_SetMaterial : MonoBehaviour {
+
+    //public Transform cube;
+
+   // Transform t = prefabObject.GetComponentInChildren<Transform>().find("Child_name");
+    
+	// Use this for initialization
+	void Start () {
+
+
+        
+
+
+       // GameObject[] cubeArray = GameObject.FindGameObjectsWithTag("cube");
+
+       /* foreach (GameObject cube in cubeArray)
+        {
+            Transform top = cube.GetComponentInChildren<Transform>().Find("Top");
+            Transform bottom = cube.GetComponentInChildren<Transform>().Find("Bottom");
+            Transform front = cube.GetComponentInChildren<Transform>().Find("Front");
+            Transform back = cube.GetComponentInChildren<Transform>().Find("Back");
+            Transform right = cube.GetComponentInChildren<Transform>().Find("Right");
+            Transform left = cube.GetComponentInChildren<Transform>().Find("Left");
+        }*/
+        
+        
+     /*   var go = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        var rend = go.GetComponent<Renderer>();
+        rend.material.mainTexture = Resources.Load("Grass_Top") as Texture;
+	*/
+      }
+	
+	// Update is called once per frame
+	void Update () {
+
+       
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject cube;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Instantiate(this, new Vector3(0, 1, 0), Quaternion.identity);
+
+            Instantiate(cube, new Vector3((Random.Range(-5, 5)), (Random.Range(-5, 5)), (Random.Range(-5, 5))), Quaternion.identity);
+            
+            
+            //GameObject[] cubeArray = GameObject.FindGameObjectsWithTag("cube");
+
+            /* foreach (GameObject cube in cubeArray)
+             {
+                 Transform top = cube.GetComponentInChildren<Transform>().Find("Top");
+                 Transform bottom = cube.GetComponentInChildren<Transform>().Find("Bottom");
+                 Transform front = cube.GetComponentInChildren<Transform>().Find("Front");
+                 Transform back = cube.GetComponentInChildren<Transform>().Find("Back");
+                 Transform right = cube.GetComponentInChildren<Transform>().Find("Right");
+                 Transform left = cube.GetComponentInChildren<Transform>().Find("Left");
+             }*/
+        }
+	}
+}
