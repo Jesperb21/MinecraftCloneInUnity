839b449c7d92154a56382331b5fc599c31045dfe
Jesperb21 <Jesper.j.b.baunsgaard@gmail.com>
2015-06-19 18:35:02 +0200

    "slight" performance increases for the world generator
    now the world generator uses co-routines to generate different parts of
    the chunks(the actual chunk and a nested co-routine that creates
    columns, can even tweak it so it creates rows (by setting an optional
    boolean to false on line 64) , decreased the number of chunks being
    generated at once to 1;  well... it doesn't go stuck generating the
    chunks now... not it just cant handle 1k game objects on the screen  per
    chunk... better look into that

--- Assets/Scripts/WorldGen/Generators/WorldGenerator.cs
+++ Assets/Scripts/WorldGen/Generators/WorldGenerator.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using SimplexNoise;
 using System.Collections.Generic;
+using System.Threading;
 
 //not really a Perlin Noise generator anymore
 public class WorldGenerator : MonoBehaviour
@@ -16,39 +17,58 @@ public class WorldGenerator : MonoBehaviour
     public int heightLimit = 128;
     public Vector2 WorldSize = new Vector2(2, 2);
 
+    private bool generatingChunk = false;
+
     // Use this for initialization
     void Start()
     {
-
+        StartCoroutine(GenerateChunksOverTime());   
+    }
+    IEnumerator GenerateChunksOverTime()
+    {
         for (int x = 0; x < WorldSize.x; x++)
         {
             for (int z = 0; z < WorldSize.y; z++)
             {
+                while (generatingChunk)
+                {
+                    yield return new WaitForSeconds(5f);
+                }
                 GameObject chunk = new GameObject("chunk_x" + x + "_z" + z);
                 chunk.transform.parent = transform;
                 chunk.transform.position = new Vector3(x * chunkSize, 0f, z * chunkSize);
-                GenerateChunk(x, z, chunk);
+
+                int actualChunkX = x * chunkSize;
+                int actualChunkZ = z * chunkSize;
+
+                StartCoroutine(CreateChunksOverTime(actualChunkX, actualChunkZ, chunk));
             }
         }
-        GameObject manualChunk = new GameObject("chunk_x" + 10 + "_z" + 10);
-        manualChunk.transform.parent = transform;
-        manualChunk.transform.position = new Vector3(10 * chunkSize, 0f, 10 * chunkSize);
-        GenerateChunk(10, 10, manualChunk);
+        yield return null;
     }
 
-    void GenerateChunk(int chunkX, int chunkZ, GameObject chunkObject)
+    
+
+    /// <summary>
+    /// generates chunks over time, attmepting to stay at 60fps while generating chunks
+    /// </summary>
+    /// <param name="actualChunkX">actual x coords of the chunk</param>
+    /// <param name="actualChunkZ">actual z coords of the chunk</param>
+    /// <param name="chunkObject">the gameObject to add all the blocks to</param>
+    /// <param name="generateColumnsPerFrame">generate columns per frame, if this is set to true it will generate columns, 
+    /// if false it will generate rows of columns per frame; generating columns per frame results in the fastest 
+    /// appearance of grass blocks, but generating rows per frame (set to false) results in the fastest overall generation
+    /// of the terrain, might use more cpu power while generating, but will be faster.... ultimately generating columns 
+    /// per frame looks coolest, so i'll enable that per default ;)</param>
+    /// <returns>nothing of note, IEnumerator's are used for coroutines</returns>
+    IEnumerator CreateChunksOverTime(int actualChunkX, int actualChunkZ, GameObject chunkObject, bool generateColumnsPerFrame = true)
     {
-        int actualChunkX = chunkX * chunkSize;
-        int actualChunkZ = chunkZ * chunkSize;
-        //float y;
-        GameObject objToMake = stone; //default block stone, if nothing else is selected make this
-        GameObject[] LineOfBlocks = new GameObject[512];
-        float[] Layer = new float[3];
-        bool generate = false;
+        generatingChunk = true;
         for (int x = 0; x < chunkSize; x++)
         {
             for (int z = 0; z < chunkSize; z++)
             {
+                #region comments
                 //these are probably a bit better for plains-ish biomes
                 //explanations of values in these lines
                 //argument 1: x+actualChunkX to get the actual x value in world position of the chunk
@@ -60,9 +80,27 @@ public class WorldGenerator : MonoBehaviour
                 //argument 4: smoothness of the terrain, larger = less noisy
                 //argument 5:max height of hills
                 //argument 6: exponent, usefull for creating larger cliffs without changing too much on arg 4 & 5 (has the exact same effect though
-                int stoneHeightBorder = SimplexNoise((x + actualChunkX), 100, (z + actualChunkZ), 10, 3, 1.2f);//controls "hills" in the stone
-                stoneHeightBorder += SimplexNoise((x + actualChunkX), 300, (z + actualChunkZ), 20, 2, 0) + 10; // controls the main levels of stone
+                #endregion
+                
+                int stoneHeightBorder = 0;
+                //make a new thread to calculate StoneHeightBorder...
+                Thread t = new Thread(() =>
+                    {
+                        stoneHeightBorder = SimplexNoise((x + actualChunkX), 100, (z + actualChunkZ), 10, 3, 1.2f);//controls "hills" in the stone
+                        stoneHeightBorder += SimplexNoise((x + actualChunkX), 300, (z + actualChunkZ), 20, 2, 0) + 10; // controls the main levels of stone
+                    });
+                t.Start();
+                //while waiting for this line to be calculated (which is probably faster)
                 int dirtHeightBorder = SimplexNoise((x + actualChunkX), 40, (z + actualChunkZ), 80, 10, 0) + 3;
+                //and then joining them together
+                t.Join();
+
+                //this is all done to because both calculations are some fairly heavy calculations, doing them this way seperates them 
+                //out onto different CPU cores... probably not any noticeable performance increase... i get the feeling it would actually
+                //use more cpu power to generate the new thread than it would to run both calculations on the same core, but this is a 
+                //attempt to make the simplex noise generation slightly more efficient
+
+                #region fancy values
                 /*
                 quite nice values, look for better alternatives though: 
                 int stoneHeightBorder = SimplexNoise((x+actualChunkX), 0, (z+actualChunkZ), 10, 3, 1.2f);
@@ -70,51 +108,87 @@ public class WorldGenerator : MonoBehaviour
                 int dirtHeightBorder = SimplexNoise((x+actualChunkX), 100, (z+actualChunkZ), 50, 2, 0) + 1;
                  
                  */
+                #endregion
+
                 if (dirtHeightBorder < 1)
                     dirtHeightBorder = 1;
+                
+                //generate columns, over time so it doesn't freeze the entire pc while its generating
+                StartCoroutine(CreateColumnsOverTime(stoneHeightBorder, dirtHeightBorder, (x + actualChunkX), (z + actualChunkZ), chunkObject));
+                
+                if (generateColumnsPerFrame)
+                    yield return new WaitForEndOfFrame();
 
-                for (int y = 0; y < heightLimit; y++)
+            }
+            yield return new WaitForEndOfFrame();
+        }
+        generatingChunk = false;
+    }
+
+    IEnumerator CreateColumnsOverTime(int stoneHeightBorder, int dirtHeightBorder, int x, int z, GameObject chunkObject)
+    {
+        GameObject objToMake;
+        bool generate = false;
+        int y = 0;
+        int maxPerFrame = 1;
+        int airblocksInARow = 0;
+
+        while (y < heightLimit)
+        {
+            for (int spawned = 0; spawned < maxPerFrame && y < heightLimit; spawned++)
+            {
+                if (y <= stoneHeightBorder)
                 {
-                    if (y <= stoneHeightBorder)
-                    {
-                        objToMake = stone;
-                        float f = SimplexNoiseFloat(x, y, z, 10, 2, 0);
-                        //1.7 seems reasonable for iron or coal veins
-                        //the closer to 2 the rarer stuff is
-                        //1.94 seems the highest rarity generated in a 3x3 area of chunks with a chunksize of 32
-                        //that might be usefull for diamonds, and 1.95 for emeralds
+                    objToMake = stone;
+                    float f = SimplexNoiseFloat(x, y, z, 10, 2, 0);
+                    //1.7 seems reasonable for iron or coal veins
+                    //the closer to 2 the rarer stuff is
+                    //1.94 seems the highest rarity generated in a 3x3 area of chunks with a chunksize of 32
+                    //that might be usefull for diamonds, and 1.95 for emeralds
 
-                        if (f > 1.9)
-                        {
-                            //Debug.Log(f);
-                            objToMake = emeraldOre;
-                        }
-                        generate = true;
-                    }
-                    else if (y <= stoneHeightBorder + dirtHeightBorder)
+                    if (f > 1.9)
                     {
-                        objToMake = dirt;
-                        generate = true;
+                        //Debug.Log(f);
+                        objToMake = emeraldOre;
                     }
-                    else if (y <= stoneHeightBorder + dirtHeightBorder + 1)
+                    generate = true;
+                }
+                else if (y <= stoneHeightBorder + dirtHeightBorder)
+                {
+                    objToMake = dirt;
+                    generate = true;
+                    airblocksInARow = 0;
+                }
+                else if (y <= stoneHeightBorder + dirtHeightBorder + 1)
+                {
+                    objToMake = grass;
+                    generate = true;
+                    airblocksInARow = 0;
+                }
+                else
+                {
+                    airblocksInARow++;
+                    objToMake = null;
+                    generate = false;
+                    if (airblocksInARow > 10)
                     {
-                        objToMake = grass;
-                        generate = true;
-                    }
-                    else
-                    {
-                        generate = false;
-                    }
-                    if (generate)
-                    {
-                        GameObject c = (GameObject)Instantiate(objToMake, new Vector3(actualChunkX + x, y, actualChunkZ + z), Quaternion.identity);
-                        c.transform.parent = chunkObject.transform;
+                        spawned = maxPerFrame;
+                        y = heightLimit;
+                        //Debug.Log("done");
                     }
                 }
+                if (generate)
+                {
+                    GameObject c = (GameObject)Instantiate(objToMake, new Vector3(x, y, z), Quaternion.identity);
+                    c.transform.parent = chunkObject.transform;
+                }
+                y++;
             }
+            yield return new WaitForEndOfFrame();
         }
 
     }
+
     /// <summary>
     /// Simplex Noise generation method, calls SimplexNoiseFloat and rounds the result to nearest integer
     /// </summary>
@@ -127,10 +201,8 @@ public class WorldGenerator : MonoBehaviour
     /// <returns>the calculated simplex noise value as an integer</returns>
     public int SimplexNoise(int x, int y, int z, float scale, float height, float power)
     {
-        float rValue;
-        rValue = SimplexNoiseFloat(x, y, z, scale, height, power);
+        float rValue = SimplexNoiseFloat(x, y, z, scale, height, power);
         return Mathf.RoundToInt(rValue);
-
     }
     //simplex noise generation method, returns a float
     public float SimplexNoiseFloat(int x, int y, int z, float scale, float height, float power)
